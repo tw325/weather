7e2b922bba9e2768a49e7bee6c134d94ac80f2e5
Tyler Wang <twang.123@live.com>
2017-04-06 16:18:07 -0400

    complete steps 1-5, additionally implemented Date

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Weather Forecast</title>
+  <!--<link rel="stylesheet" type="text/css" href="css/style.css">-->
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="js/script.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <div class="banner">
+    <h1>AERIS WEATHER FORECAST</h1>
+  </div>
+  <div class="content" id="week">
+  </div>
+
+</body>
+
+</html>
---
+++ js/script.js
@@ -0,0 +1,44 @@
+$.getJSON( "http://api.aerisapi.com/forecasts/11101?client_id=mrWm7AFVW86e9GKKX4xpJ&client_secret=t0UN1tmEkyOaHGuNFlzOZaPXOXuXuaXzW396RBLa", function( data ) {
+  var periods = data.response[0].periods;
+  var items = [];
+  var monthDict = {
+    1 : "January",
+    2 : "February",
+    3 : "March",
+    4 : "April",
+    5 : "May",
+    6 : "June",
+    7 : "July",
+    8 : "August",
+    9 : "September",
+    10 : "October",
+    11 : "November",
+    12 : "December"
+  };
+  var weekArray = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+
+
+  $.each( periods, function( index, val ) {
+    items.push("<tr>");
+    var dateArray = val.dateTimeISO.split("-");
+    var year = parseInt(dateArray[0]);
+    var month = parseInt(dateArray[1]);
+    var day = parseInt(dateArray[2].substring(0,2));
+    console.log(val.dateTimeISO.substring(0,10) + " " + day);
+    var date = new Date(val.dateTimeISO.substring(0,10));
+    var dayOfWeek = date.getDay();
+    console.log(dayOfWeek);
+
+    items.push("<td>" + "<b>"+weekArray[dayOfWeek]+"</b><br> "+monthDict[month]+" "+day+", "+year + "</td>");
+    items.push("<td>HIGH: " + val.maxTempF + "</td>");
+    items.push("<td>LOW: " + val.minTempF + "</td>");
+    items.push("<td><img src=\"img/" + val.icon + "\"> </td>");
+    items.push("</tr>");
+  });
+
+  $( "<table/>", {
+    "class": "forecast",
+    html: items.join( "" )
+  }).appendTo( "#week" );
+});
